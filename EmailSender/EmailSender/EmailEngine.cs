3b0c1fe05c577f9b2cc1af327a21c1117bffef83
afeexclusive <afeexclusive@gmail.com>
2023-05-22 22:38:31 +0100

    unhide engine from git ignore

---
+++ EmailSender/EmailSender/EmailEngine.cs
@@ -0,0 +1,179 @@
+﻿using MimeKit;
+using PD.EmailSender.Helpers.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+
+namespace PD.EmailSender.Helpers
+{
+    public class EmailEngine
+    {
+        public static bool SendEmail(MessageModel msgModel, SenderSettings sender)
+        {
+            MimeMessage message = new MimeMessage();
+            BodyBuilder builder = new BodyBuilder();
+            if (msgModel.Attachments != null)
+            {
+                foreach (var item in msgModel.Attachments)
+                {
+                    string fileName = item.FileName;
+                    Stream fileStream = item.OpenReadStream();
+                    builder.Attachments.Add(fileName, fileStream);
+
+                }
+            }
+
+            if (msgModel.AttachmentInCode != null)
+            {
+                foreach (var item in msgModel.AttachmentInCode)
+                {
+                    string fileName = string.IsNullOrWhiteSpace(msgModel.Filename) ? $"PdDocs{msgModel.AttachmentInCode.FindIndex(x => x.Equals(item))}" : msgModel.Filename;
+                    builder.Attachments.Add(fileName, item);
+
+                }
+            }
+
+            builder.HtmlBody = msgModel.Message;
+
+            message.Body = builder.ToMessageBody();
+            message.From.Add(new MailboxAddress(msgModel.EmailDisplayName, sender.Email));
+            message.ReplyTo.Add(new MailboxAddress(msgModel.EmailDisplayName, msgModel.ReplyTo ?? sender.Email));
+            msgModel.EmailAddresses?.ToList().ForEach(contact=> message.To.Add(MailboxAddress.Parse(contact)));
+            msgModel.Bcc?.ToList().ForEach(x => message.Bcc.Add(MailboxAddress.Parse(x)));
+            msgModel.Cc?.ToList().ForEach(x => message.Cc.Add(MailboxAddress.Parse(x)));
+            message.Subject = msgModel.Subject;
+            return SendReadyEmail(message, sender);
+        }
+
+        public static List<SenderSettings> AuthenticateSender(string emailaddress, string password, List<CommonHosts> commonHosts, string domain = "", int port = 0)
+        {
+            // Handle specified domain
+            if (!string.IsNullOrWhiteSpace(domain))
+            {
+                CommonHosts specified = new CommonHosts
+                {
+                    Domain = domain,
+                    Ports = new int[] { port },
+                    ServerType = "specified",
+                    ServiceName = "Custome"
+                };
+                bool isAuth = Authenticator(emailaddress, password, specified);
+                if (isAuth)
+                    return new List<SenderSettings>() { new SenderSettings { Domain = domain, Email = emailaddress, Passord = password, Port = port } };
+
+
+                return null;
+            }
+
+            //Handle anonymous domain
+            CommonHosts anonymuosdomain = new CommonHosts
+            {
+                Domain = emailaddress.Split("@")[1],
+                Ports = new int[] { 0, 465, 587, 2525, 25 },
+                ServerType = "anon",
+                ServiceName = "Custom"
+            };
+
+            //Add anonymous domains to default domains
+            commonHosts.Add(anonymuosdomain);
+
+            if (string.IsNullOrWhiteSpace(emailaddress) && !emailaddress.Contains("@"))
+                return null;
+
+            if (string.IsNullOrWhiteSpace(password))
+                return null;
+
+            //Iterate all hosts to get the hosts to use for auth. Default host is added already
+            List<CommonHosts> hostForAuth = new List<CommonHosts>();
+            foreach (var host in commonHosts)
+            {
+                List<CommonHosts> hostPerPort = AuthDetails(host);
+                hostForAuth.AddRange(hostPerPort);
+            }
+
+            // tests all ports
+            Task<bool>[] tasks = hostForAuth.Select(a => Task<bool>.Factory.StartNew(() => Authenticator(emailaddress, password, a))).ToArray();
+            Task.WaitAll(tasks);
+            //then add the result of all the tasks to r in a treadsafe fashion
+            List<bool> authResult = tasks.Select(task => task.Result).ToList();
+
+            // pick those that connect and return the connection settings
+            List<SenderSettings> connectionList = new List<SenderSettings>();
+
+            for (int i = 0; i < authResult.Count; i++)
+            {
+                if (authResult[i])
+                {
+                    connectionList.Add(
+                        new SenderSettings
+                        {
+                            Domain = hostForAuth[i].Domain,
+                            Email = emailaddress,
+                            Passord = password,
+                            Port = hostForAuth[i].Ports[0]
+                        });
+                }
+            }
+            return connectionList;
+        }
+
+        private static List<CommonHosts> AuthDetails(CommonHosts oneDetails)
+        {
+            List<CommonHosts> domainlist = new List<CommonHosts>();
+            foreach (var item in oneDetails.Ports)
+            {
+                domainlist.Add(new CommonHosts
+                {
+                    Domain = oneDetails.Domain,
+                    Ports = new int[] { item },
+                    ServerType = oneDetails.ServerType,
+                    ServiceName = oneDetails.ServiceName
+                });
+            }
+            return domainlist;
+        }
+
+
+        private static bool Authenticator(string emailaddress, string password,  CommonHosts details)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            try
+            {
+                smtpClient.Connect(details.Domain, details.Ports.First(), true);
+                smtpClient.Authenticate(emailaddress, password);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static bool SendReadyEmail(MimeMessage message, SenderSettings sender)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            try
+            {
+                //projectdriveng.com.ng
+                smtpClient.Connect(sender.Domain, sender.Port, true);
+                smtpClient.Authenticate(sender.Email, sender.Passord);
+                smtpClient.Send(message);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string errInfor = ex.Message;
+                return false;
+            }
+            finally
+            {
+                smtpClient.Disconnect(true);
+                smtpClient.Dispose();
+            }
+        }
+    }
+}
